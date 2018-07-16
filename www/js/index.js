2b6758028023c0c7565e729dfe6d32a94fff1931
DESKTOP-DQ3ODV6\MARIEYFRAN <falvarino@ximiodev.com>
2018-07-16 13:05:03 -0300

    alertas

--- www/js/index.js
+++ www/js/index.js
@@ -58,49 +58,56 @@ var app = {
 				});
 			  
 			window.FirebasePlugin.getToken(function(token) {
-				console.log(token);
-					
-					
-				var user_platform = device.platform;
-				console.log('registration event: ' + token);
-				var datos = {
-					'accion':'registrarDev',
-					'user_platform': user_platform,
-					'registrationId': token
-				}
-				$.ajax({
-					type: 'POST',
-					data: datos,
-					dataType: 'json',
-					url: baseURL,
-					success: function (data) {
-						if(data.res) {
-							alert(data.res);
-						}
-					},
-					error      : function(xhr, ajaxOptions, thrownError) {
-						alert("error 216");
-					}
-					
-				});
-				var oldRegId = localStorage.getItem('registrationId');
-				if (oldRegId !==token) {
-					// Save new registration ID
-					localStorage.setItem('registrationId', token);
-					// Post registrationId to your app server as the value has changed
-				}
-
-				var parentElement = document.getElementById('registration');
-				var listeningElement = parentElement.querySelector('.waiting');
-				var receivedElement = parentElement.querySelector('.received');
-
-				listeningElement.setAttribute('style', 'display:none;');
-				receivedElement.setAttribute('style', 'display:block;');
+				salvtoken(token);
 			}, function(error) {
-				console.error(error);
+				alert(error);
+			});
+			window.FirebasePlugin.onTokenRefresh(function(token) {
+				// save this server-side and use it to push notifications to this device
+				salvtoken(token);
+			}, function(error) {
+				alert(error);
 			});
 		} catch(e) {
 			alert(e);
 		}
     }
 };
+
+function salvtoken(token) {
+	var user_platform = device.platform;
+	alert('registration event: ' + token);
+	var datos = {
+		'accion':'registrarDev',
+		'user_platform': user_platform,
+		'registrationId': token
+	}
+	$.ajax({
+		type: 'POST',
+		data: datos,
+		dataType: 'json',
+		url: baseURL,
+		success: function (data) {
+			if(data.res) {
+				alert(data.res);
+			}
+		},
+		error      : function(xhr, ajaxOptions, thrownError) {
+			alert("error 216");
+		}
+		
+	});
+	var oldRegId = localStorage.getItem('registrationId');
+	if (oldRegId !==token) {
+		// Save new registration ID
+		localStorage.setItem('registrationId', token);
+		// Post registrationId to your app server as the value has changed
+	}
+
+	var parentElement = document.getElementById('registration');
+	var listeningElement = parentElement.querySelector('.waiting');
+	var receivedElement = parentElement.querySelector('.received');
+
+	listeningElement.setAttribute('style', 'display:none;');
+	receivedElement.setAttribute('style', 'display:block;');
+}
